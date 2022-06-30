4099b259a6c5c3634975400904ee8e246c03ae7f
Julia Krapivnaya <y.krapivnaya@gmail.com>
2022-06-30 20:32:33 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,44 @@
+$(function () {
+
+
+    /* fixed header*/
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+
+            console.log(scrollPos);
+
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /*smooll scroll*/
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+        console.log(elementOffset);
+
+        $("html,body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+
+});
